fa0ff1fe1ca913fe5f4f607927451775b369a56d
mashish9092 <156409332+mashish9092@users.noreply.github.com>
2026-05-02 17:20:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Survey Data API (Dashboard + Entry)
 const API_MAIN = "https://script.google.com/macros/s/AKfycby2y_dAYeVHGTTs56zCrK-ROzoBNt8G8j6wvUsBQgme8FV3ZYvuxy8u0lC5skRIal-p/exec"; 
-const API = "https://script.google.com/macros/s/AKfycbykf4R_ezqdOhQrb3BOLlcmqlaDRnOe1KPnK-wwG1O5twuimU5RVqgjgbLD-jMx3jEl/exec"; 
+const API_REGISTER = "https://script.google.com/macros/s/AKfycbykf4R_ezqdOhQrb3BOLlcmqlaDRnOe1KPnK-wwG1O5twuimU5RVqgjgbLD-jMx3jEl/exec"; 
