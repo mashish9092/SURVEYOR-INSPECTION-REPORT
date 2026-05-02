3fd1cfe2c0994c0d54e2bc3594a989e2686c3c10
mashish9092 <156409332+mashish9092@users.noreply.github.com>
2026-05-02 17:14:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const API = "https://script.google.com/macros/s/AKfycby2y_dAYeVHGTTs56zCrK-ROzoBNt8G8j6wvUsBQgme8FV3ZYvuxy8u0lC5skRIal-p/exec";
-const API = "https://script.google.com/macros/s/AKfycbykf4R_ezqdOhQrb3BOLlcmqlaDRnOe1KPnK-wwG1O5twuimU5RVqgjgbLD-jMx3jEl/exec";
+const API = "https://script.google.com/macros/s/AKfycby2y_dAYeVHGTTs56zCrK-ROzoBNt8G8j6wvUsBQgme8FV3ZYvuxy8u0lC5skRIal-p/exec"; /*All Page API*/
+const API = "https://script.google.com/macros/s/AKfycbykf4R_ezqdOhQrb3BOLlcmqlaDRnOe1KPnK-wwG1O5twuimU5RVqgjgbLD-jMx3jEl/exec"; /*📘 खरीदी रजिस्ट API*/
