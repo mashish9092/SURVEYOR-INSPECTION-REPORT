53d1dd463547613d4dd0d01ba98dd82299edec2c
mashish9092 <156409332+mashish9092@users.noreply.github.com>
2026-05-02 13:33:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API = "https://script.google.com/macros/s/AKfycbz9metOYGoz3bELs6l7ybNBBgR6vsSYEvRt4c2fRYQUEh19f3_jWr2hEtk8TJtRvej5/exec";
+const API = "https://script.google.com/macros/s/AKfycbxVNmOjc_jFibxGwrvD4mlOGXASBXbxFUJagfJ3_5qfo4GvR4TxaOvRUW6bMGW2QaXQ/exec";
