a30fbd6f05c80b3d98afa011f2517689665ecf18
mashish9092 <156409332+mashish9092@users.noreply.github.com>
2026-05-03 18:04:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // Survey System API (Dashboard + Entry)
-const API_MAIN = "https://script.google.com/macros/s/AKfycbyN5sJfMmnfRVmJWAlo5okx8jUpq_G1IGOWza2hTOBIFrytY7l43uPOrl2-N6NT9nNi/exec";
-
+const API_MAIN = "https://script.google.com/macros/s/AKfycbyN5sJfMmnfRVmJWAlo5okx8jUpq_G1IGOWza2hTOBIFrytY7l43uPOrl2-N6NT9nNi/exec?v=1";
 // खरीदी रजिस्टर API
 const API_REGISTER = "https://script.google.com/macros/s/AKfycbykf4R_ezqdOhQrb3BOLlcmqlaDRnOe1KPnK-wwG1O5twuimU5RVqgjgbLD-jMx3jEl/exec";
